9efe71cb60bea6071bf677a4ca9f7077c645839b
Owais Salim <salimowais007@gmail.com>
2022-01-06 21:52:16 +0530

    Milestone1_Q8

--- #1 Goldmann Sachs/08_total_decoding_messages.py
+++ #1 Goldmann Sachs/08_total_decoding_messages.py
@@ -4,26 +4,26 @@ class Solution:
 	def CountWays(self, str):
 		# Code here
 		if len(str) == 0:
-            return 1
-        if str[0] == '0':
-            return 0
-		n = len(str)
-		p = q = res = 1
-		for i in range(1, n):
-		    t = (int(str[i-1])-0)*10 + int(str[i]) - 0
-		    if t >= 10 and t <= 26:
-		        if t == 10 or t == 20:
-		            res = p
-	            else:
-	                res = p + q
-	        else:
-	            if t % 10 == 0:
-	                return 0
-	            else:
-	                res = q
-	        p = q
-	        q = res
-        return res % (10**9 + 7)
+                    return 1
+                if str[0] == '0':
+                    return 0
+                        n = len(str)
+                        p = q = res = 1
+                        for i in range(1, n):
+                            t = (int(str[i-1])-0)*10 + int(str[i]) - 0
+                            if t >= 10 and t <= 26:
+                                if t == 10 or t == 20:
+                                    res = p
+                            else:
+                                res = p + q
+                        else:
+                            if t % 10 == 0:
+                                return 0
+                            else:
+                                res = q
+                        p = q
+                        q = res
+                return res % (10**9 + 7)
 
 #{ 
 #  Driver Code Starts
