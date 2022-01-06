055b44496621dcc66f0c136d8e0431a6e8fe3251
Owais Salim <salimowais007@gmail.com>
2022-01-06 16:27:45 +0530

    Milestone1_Q5

---
+++ #1 Goldmann Sachs/05_nth_ugly_number.py
@@ -0,0 +1,35 @@
+#User function Template for python3
+class Solution:
+	def getNthUglyNo(self,n):
+		# code here
+		ugly = [1]
+        i2, i3, i5 = 0, 0, 0
+        while n > 1:
+            u2, u3, u5 = 2 * ugly[i2], 3 * ugly[i3], 5 * ugly[i5]
+            umin = min((u2, u3, u5))
+            if umin == u2:
+                i2 += 1
+            if umin == u3:
+                i3 += 1
+            if umin == u5:
+                i5 += 1
+            ugly.append(umin)
+            n -= 1
+        return ugly[-1]
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        ob = Solution()
+        ans = ob.getNthUglyNo(n);
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
